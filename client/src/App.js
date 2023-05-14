0b3ccb942d1d9077ae5a30967705d7bfc9f0597c
Amanda Contreras <amandaben21@aol.com>
2023-05-14 00:08:00 -0400

    Made a bunny form

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import { useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+// import {BrowserRouter} from 'react-router-dom'; is in src folder-> in index.js file
+import BunnyForm from './components/BunnyForm';
 
 function App() {
+
+  useEffect(() => {
+  axios.get("http://localhost:8000/api/bunnies")
+  .then(res => console.log(res.data))
+  .catch(err => console.log(err))
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Routes>
+        < Route element={<BunnyForm/>} path="api/bunnies/create" />
+      </Routes> */}
+      <BunnyForm/>
     </div>
   );
 }
